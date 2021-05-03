69328457e1d2c4e3cdbe31d2a5e2accd77a3b092
Altamas <altmasalam00@gmai.com>
2021-05-03 12:48:59 +0530

    Navbar update

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+
+
+const Navbar = () =>{
+
+    return(
+
+    <nav>
+        <ul className="left"> 
+            <li><a href="">IndiaExpress </a></li>
+        </ul>
+        <ul className="right">
+            <li><a href="">
+                <span className="showppingCard"><i class="fas fa-cart-arrow-down"></i>
+                <span className="cardCount">0 </span></span>
+                </a></li>
+        </ul>
+    </nav>
+    )
+}
+
+export default Navbar;
