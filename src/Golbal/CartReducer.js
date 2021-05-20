c329301f9211e546a9884496199ba8ea89f1a49b
Altamas <altmasalam00@gmai.com>
2021-05-20 14:04:41 +0530

    update cart incrimant and decriment

--- src/Golbal/CartReducer.js
+++ src/Golbal/CartReducer.js
@@ -19,6 +19,44 @@ export const CartReducer = (state,action) =>{
                 updatePrice,qty:updateQty}
             }
             break;
+            case'INC':
+            product=action.cart;
+            product.qty = product.qty+1;
+            updatePrice = totalPrice + product.price;
+            updateQty = qty + 1;
+            index = shoppingCart.findIndex(cart => cart.id === action.id);
+            shoppingCart[index] = product;
+            return{shoppingCart:[...shoppingCart],totalPrice:updatePrice,
+            qty:updateQty}
+            break;
+            case'DEC':
+            product=action.cart;
+            if(product.qty > 1){
+
+                product.qty = product.qty-1;
+            updatePrice = totalPrice - product.price;
+            updateQty = qty - 1;
+            index = shoppingCart.findIndex(cart => cart.id === action.id);
+            shoppingCart[index] = product;
+            return{shoppingCart:[...shoppingCart],totalPrice:updatePrice,
+             qty:updateQty}
+            }else{
+                return state;
+            }
+            break;
+            case 'DELETE':
+            const filtered = shoppingCart.filter(product => product.id !== action.id)
+            product = action.cart;
+            updateQty = qty - product.qty;
+            updatePrice = totalPrice - product.price*product.qty;
+            return{shoppingCart:[...filtered],totalPrice,qty:updateQty}
+            break;
+
+              
+
+
+
+
             default:return state;
             
     }
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -16,11 +16,11 @@ const Cart = () =>
                    <span className="cart-image"><img src={cart.Image} alt="not found" /> </span>
                    <span className="cart-product-name">{cart.name}</span>
                    <span className="cart-product-price">${cart.price}.00</span>
-                   <span className="inc"><i className="fas fa-plus"></i></span>
+                   <span className="inc" onClick={() =>dispatch({type:'INC',id:cart.id,cart})}><i className="fas fa-plus"></i></span>
                    <span className="product-quantity">{cart.qty}</span>
-                   <span className="dec"><i className="fas fa-minus"></i></span>
-                   <span className="product-total-price">500:00</span>
-                   <span className="delete-product"><i className="far fa-trash-alt"></i></span>
+                   <span className="dec"onClick={()=>dispatch({type:'DEC',id:cart.id,cart})}><i className="fas fa-minus"></i></span>
+                   <span className="product-total-price">${cart.price*cart.qty}.00</span>
+                   <span className="delete-product" onClick ={()=> dispatch({type:'DELETE',id:cart.id,cart})}><i className="far fa-trash-alt"></i></span>
                 </div>
             ))
             :'your cart is empty'}
